766fea80eb3f8bc39ad800ca5d23ddfd102f8834
DanielVL11 <danyinfo158@gmail.com>
2024-03-11 14:17:37 -0600

    Inicio de Proyecto
    Creación del proyecto MAD para dar solución a las incidencias que ocurren en las aulas del tecnológico de tlaxiaco.

---
+++ source/repos/PruebaMAD/PruebaMAD/Program.cs
@@ -0,0 +1,17 @@
+﻿using LibreriaMAD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaMAD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Registro.ValoresR());
+        }
+    }
+}
